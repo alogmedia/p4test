2eb2a2628698f6b6cc8878d2117398d33d874bbf
alogmedia <albertlogn@gmail.com>
2023-10-04 08:46:37 +0200

    Update til overlay.

--- assets/js/overlay.js
+++ assets/js/overlay.js
@@ -1,17 +1,27 @@
-// Get the modal and the CTA button elements
-const modal = document.getElementById('cityModal');
+// Get both modal elements and the CTA button elements
+const cityModal = document.getElementById('cityModal');
+const infoModal = document.getElementById('infoModal');
 const ctaButtons = document.querySelectorAll('.cta-button');
 
-// Open the modal when any "Lej Stand" button is clicked
+// Open the cityModal when any "Lej Stand" button is clicked
 ctaButtons.forEach(btn => {
-    btn.addEventListener('click', function() {
-        modal.style.display = 'flex';
+    btn.addEventListener('click', function(event) {
+        // Prevent event propagation to ensure the window click event doesn't interfere
+        event.stopPropagation();
+        if (btn.textContent === "Videre") {
+            // Switch to the second modal when "Videre" is clicked
+            cityModal.style.display = 'none';
+            infoModal.style.display = 'flex';
+        } else if (btn.textContent === "Lej Stand" || btn.textContent === "Call To Action") {
+            cityModal.style.display = 'flex';
+        }
     });
 });
 
-// Close the modal when clicking outside the modal content
+// Close the modals when clicking outside the modal content
 window.addEventListener('click', function(event) {
-    if (event.target === modal) {
-        modal.style.display = 'none';
+    if (event.target === cityModal || event.target === infoModal) {
+        cityModal.style.display = 'none';
+        infoModal.style.display = 'none';
     }
 });
